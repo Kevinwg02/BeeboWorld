df48e8878725515b9fc82708fb4961f3d60455fe
Kevinwg02 <71637950+Kevinwg02@users.noreply.github.com>
2024-08-12 11:43:10 +0200

    Update index.php
    prendre en compte les nouvelles tables

--- index.php
+++ index.php
@@ -18,17 +18,17 @@
             </div>
             <div class="all">
                 <h2 class="titre"><?php echo htmlspecialchars($row['titre']); ?></h2>
-                <p class="auteur">Author: <?php echo htmlspecialchars($row['auteur']); ?></p>
+                <p class="auteur">Auteur: <?php echo htmlspecialchars($row['auteur']); ?></p>
                 <p class="saga">Saga: <?php echo htmlspecialchars($row['saga']); ?></p>
                 <p class="isbn">ISBN: <?php echo htmlspecialchars($row['isbn']); ?></p>
-                <p class="prix">Price: $<?php echo htmlspecialchars($row['prix']); ?></p>
-                <p class="maisonEdition">Publishing House: <?php echo htmlspecialchars($row['maisonEdition']); ?></p>
-                <p class="classe">Class: <?php echo htmlspecialchars($row['classe']); ?></p>
-                <p class="theme">Theme: <?php echo htmlspecialchars($row['theme']); ?></p>
-                <p class="datePublication">Publication Date: <?php echo htmlspecialchars($row['datePublication']); ?></p>
-                <p class="dateLecture">Date of Reading: <?php echo htmlspecialchars($row['dateLecture']); ?></p>
-                <p class="typeLivre">Type: <?php echo htmlspecialchars($row['typeLivre']); ?></p>
-                <p class="nbPage">Pages: <?php echo htmlspecialchars($row['nbPage']); ?></p>
+                <p class="prix">Prix: €<?php echo htmlspecialchars($row['prix']); ?></p>
+                <p class="maisonEdition">Maison d'édition: <?php echo htmlspecialchars($row['maisonEdition']); ?></p>
+                <p class="medaille">Medaille: <?php echo htmlspecialchars($row['medaille_nom']); ?></p>
+                <p class="theme">Theme: <?php echo htmlspecialchars($row['genre_nom']); ?></p>
+                <p class="datePublication">Date d'achat: <?php echo htmlspecialchars($row['dateAchat']); ?></p>
+                <p class="dateLecture">Date de lecture: <?php echo htmlspecialchars($row['dateLecture']); ?></p>
+                <p class="typeLivre">Type de livre: <?php echo htmlspecialchars($row['typeLivre_nom']); ?></p>
+                <p class="nbPage">Nombre de Pages: <?php echo htmlspecialchars($row['nbPage']); ?></p>
             </div>
         </div>
   <?php endwhile; ?>
