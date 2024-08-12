27a96eebbd3b571a052203da48c572eb83305d07
Kevinwg02 <71637950+Kevinwg02@users.noreply.github.com>
2024-08-12 11:41:52 +0200

    Update connexion.php
    ajout des joint pour medail, genre, typelivre

--- php/connexion.php
+++ php/connexion.php
@@ -5,7 +5,27 @@ $username = 'root';
 $password = '';
 
 $dsn = "mysql:host=$host;dbname=$dbname";
-$sql = "SELECT * FROM Books";
+// $sql = "SELECT * FROM Books";
+  $sql = "SELECT 
+    Books.*, 
+    Medaille.nom AS medaille_nom, 
+    Genre.nom AS genre_nom, 
+    typeLivre.nom AS typeLivre_nom
+FROM 
+    Books 
+LEFT JOIN 
+    Medaille 
+ON 
+    Books.medaille_id = Medaille.id
+LEFT JOIN 
+    Genre 
+ON 
+    Books.genre_id = Genre.id
+LEFT JOIN 
+    typeLivre 
+ON 
+    Books.typeLivre_id = typeLivre.id;
+";
 
 try {
   $pdo = new PDO($dsn, $username, $password);
