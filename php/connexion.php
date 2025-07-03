89974892855e430da0a739496bbde4cf02f55a3a
Kevinwg02 <71637950+Kevinwg02@users.noreply.github.com>
2025-07-03 09:23:09 +0200

    Add files via upload

---
+++ php/connexion.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'DbBeeboWorld';
+$username = 'root';
+$password = 'MSrpelAUyk2tg3oQ';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+try {
+  $pdo = new PDO($dsn, $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  die("Erreur de connexion : " . $e->getMessage());
+}
---
+++ php/menu.php
@@ -0,0 +1,30 @@
+<?php
+function afficherNavbar()
+{
+    ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="library.php">📚 Ma Bibliothèque</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="public/public_stats.php">📈 Statistiques</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-outline-light px-3 py-1" data-bs-toggle="modal" data-bs-target="#pagesModal" href="#">📖 Pages lues</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-danger" href="logout.php">🚪 Déconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <?php
+}
+?>
